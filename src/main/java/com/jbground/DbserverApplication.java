7f1ff85f8d0a4b0850abb4d2e58b0c5d73f75fc9
JeongJiSang <jsj00203@gmail.com>
2023-03-27 11:04:16 +0900

    수정

---
+++ src/main/java/com/jbground/DbserverApplication.java
@@ -0,0 +1,43 @@
+package com.jbground;
+
+
+import com.jbground.dbserver.service.UserServiceImpl;
+import com.jbground.grpc.User;
+import com.jbground.grpc.UserIdx;
+import com.jbground.grpc.UserServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class DbserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbserverApplication.class, args);
+
+//		int port = 8080;
+//		Server server = ServerBuilder.forPort(port).addService(new UserServiceImpl()).build();
+//
+//		try {
+//			server.start();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return;
+//		}
+//
+//		Runtime.getRuntime().addShutdownHook(new Thread(() ->{
+//			System.err.println("Server: Shutting down gRPC server");
+//			server.shutdown();
+//			System.err.println("Server: Server shutdown");
+//		}));
+
+
+	}
+
+}
---
+++ src/main/java/com/jbground/GrpcApplication.java
@@ -0,0 +1,34 @@
+package com.jbground;
+
+import com.jbground.dbserver.service.UserServiceImpl;
+import com.jbground.grpc.User;
+import com.jbground.grpc.UserIdx;
+import com.jbground.grpc.UserServiceGrpc;
+import io.grpc.*;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class GrpcApplication {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        Server server = ServerBuilder.forPort(port).addService(new UserServiceImpl()).build();
+
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("Server: Shutting down gRPC server");
+            server.shutdown();
+            System.err.println("Server: Server shutdown");
+        }));
+    }
+}
